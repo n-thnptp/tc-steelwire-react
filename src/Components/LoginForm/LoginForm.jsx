9fcca459984cbe38d6c11193cd35454630e1f7fe
n.thnptp <noppadol196@gmail.com>
2024-09-11 18:03:52 +0700

    ✨ Login page (Front end) done!

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Button } from '@material-tailwind/react';
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+
+const LoginForm = () => {
+  return (
+    <div className="flex flex-row h-screen items-center justify-center">
+        <form action="" className="flex flex-nowrap flex-col">
+            
+            <h1 className="mb-10 font-inter font-bold text-accent-900 text-3xl text-center items-center justify-center">TC Steel Wire</h1>
+
+            {/* username/email and password fields */}
+            <div className="grid grid-rows-2 gap-6">
+                <div className="flex w-full input-field">
+                    <MdEmail className="my-auto mx-2 text-3xl text-accent-900" />
+                    <input className="w-full h-[50px]" type="text" placeholder="Email / Username" required />
+                </div>
+                <div className="flex w-full input-field">
+                    <RiLockPasswordFill className="my-auto mx-2 text-3xl text-accent-900" />
+                    <input className="w-full h-[50px]" type="password" placeholder="Password" required />
+                </div>
+            </div>
+
+            {/* login button */}
+            <button type="submit" className="mt-6 primary-buttons">Login</button>
+
+            {/* register / forgot password page, relink href to corresponding pages later */}
+            <div className="my-2 text-xs text-gray-500 font-inter">
+                <a href="#" className="me-[290px] hover:text-gray-900">Don't have an account?</a>
+                <a href="#" className="hover:text-gray-900">Forgot password?</a>
+            </div>
+
+            {/* line separator */}
+            <div className="mt-7 line separator"> OR </div>
+
+            {/* alternate login buttons, via GOOGLE or FACEBOOK (add images later) */}
+            <div className="grid grid-cols-2 gap-5 mt-7">
+                {/* <button className="white-buttons" type="button">GOOGLE</button> */}
+                <Button href="#" size="lg" color="white" className="flex items-center justify-center">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" className="w-8 h-8"></img>
+                </Button>
+                <Button href="#" size="lg" color="white" className="flex items-center justify-center">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Facebook_icon_2013.svg" className="w-8 h-8"></img>
+                </Button>
+            </div>
+
+        </form>
+    </div>
+  )
+}
+
+export default LoginForm
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: '#FFEAC5',
+        accent: {
+          300: '#FFDBB5',
+          600: '#6C4E31',
+          900: '#603F26',
+        },
+      },
+    },
   },
   plugins: [],
-}
+});
 
