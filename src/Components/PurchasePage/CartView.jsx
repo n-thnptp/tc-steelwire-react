1e82e32a1b22eebef66206d26c7b6c70bf92d033
Thirawatz <thirawat.ku@ku.th>
2024-10-08 21:36:16 +0700

    100% PurchasePage but No payment and edit address

--- src/Components/PurchasePage/CartView.jsx
+++ src/Components/PurchasePage/CartView.jsx
@@ -26,49 +26,70 @@ const CartView = () => {
     <div className="p-6">
       <div className="flex items-center mb-6">
         <img src="/icon/Basket.png" alt="Cart Icon" className="w-6 h-6 mr-2" />
-        <h2 className="text-xl font-bold">ORDER IN CART</h2>
+        <h2 className="text-xl text-[#603F26] font-bold font-inter">ORDER IN CART</h2>
       </div>
 
-      {/* กล่องสินค้า - จำกัดความสูงและสามารถเลื่อนดูได้ */}
-      <div className="overflow-y-auto" style={{ maxHeight: '400px' }}> {/* ล็อกความสูงและเพิ่มการเลื่อน */}
+      {/* กล่อง x ครอบกล่อง r */}
+      <div className="overflow-y-auto" style={{ maxHeight: '400px' }}>
         {items.map((item, index) => (
-          <div key={index} className="flex items-start mb-3 p-2">
-            {/* แสดงรูปสินค้า */}
-            <div className="w-1/4">
-              <img src={item.image} alt="Product" className="w-full h-full rounded-lg object-cover" />
-            </div>
-
-            {/* รายละเอียดสินค้า */}
-            <div className="w-2/4 pl-4 flex flex-col justify-between ">
-              <h3 className="font-bold mb-1">{item.name}</h3>
-              <p className="text-sm mb-1">SIZE</p>
-              <p className="text-sm mb-1">WEIGHT</p>
-              <p className="text-sm mb-1">LENGTH</p>
-              <p className="text-sm mb-1">FEATURE</p>
-
-              {/* เส้นใต้ */}
-              <hr className="my-2 border-t border-gray-300" />
-
-              {/* ย้ายไอคอนลงมาไว้ข้างล่าง */}
-              <div className="flex justify-start items-center space-x-2 mt-2">
-                <button className="text-brown-500 p-2">
-                  <img src="/icon/Bookmark.png" alt="Bookmark Icon" className="w-5 h-5" />
-                </button>
-                <button className="text-red-500 p-2">
-                  <img src="/icon/Trash.png" alt="Trash Icon" className="w-5 h-5" />
-                </button>
+          // กล่อง r ครอบกล่อง i
+          <div key={index} className="mb-3 p-2 flex-wrap">
+            {/* กล่อง i ครอบกล่อง y และ g */}
+            <div className="flex flex-wrap">
+              {/* กล่อง y แสดงรูปสินค้า */}
+              <div className="w-full sm:w-1/4 flex-shrink-0 mb-6">
+                <img src={item.image} alt="Product" className="w-full h-full rounded-lg object-cover" />
               </div>
-            </div>
 
-            {/* ราคาสินค้า */}
-            <div className="w-1/4 text-right">
-              <p className="font-bold mb-1">{item.price}</p>
-              <p className="text-sm mb-1">{item.size}</p>
-              <p className="text-sm mb-1">{item.weight}</p>
-              <p className="text-sm mb-1">{item.length}</p>
-              <p className="text-sm mb-1">{item.feature}</p>
-            {/* เส้นใต้ */}
-            <hr className="my-2 border-t border-gray-300 " />
+              {/* กล่อง g ครอบกล่อง n */}
+              <div className="w-full sm:w-3/4 pl-4 flex flex-col justify-between">
+                {/* กล่อง n ครอบกล่อง z และ c */}
+                <div className="flex flex-col sm:flex-row justify-between">
+                  {/* กล่อง z รายละเอียดสินค้า */}
+                  <div className="hidden sm:block w-full sm:w-1/2 mb-2 sm:mb-0">
+                    <h3 className="text-[#603F26] font-bold font-inter mb-1">{item.name}</h3>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">SIZE: </p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">WEIGHT: </p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">LENGTH: </p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">FEATURE: </p>
+                    </div>
+                    {/* เพิ่มข้อมูลจากกล่อง c ในหน้าจอเล็ก */}
+                  <div className="block sm:hidden">
+                    <h3 className="text-[#603F26] font-bold font-inter mb-1">{item.name}</h3>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">SIZE: {item.size}</p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">WEIGHT: {item.weight}</p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">LENGTH: {item.length}</p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">FEATURE: {item.feature}</p>
+                  </div>
+      
+
+                  {/* กล่อง c ราคาสินค้า */}
+                  <div className="hidden sm:block w-full sm:w-1/2 text-right">
+                    <p className="text-[#603F26] font-bold font-inter mb-1">{item.price}</p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">{item.size}</p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">{item.weight}</p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">{item.length}</p>
+                    <p className="text-[#603F26] font-bold font-inter mb-1 opacity-50">{item.feature}</p>
+                  </div>
+                </div>
+
+                {/* กล่อง v มี GrayLine and icon */}
+                <div className="flex flex-col mt-2">
+                  {/* กล่องสำหรับเส้นใต้ */}
+                  <div className="w-full">
+                    <hr className="border-t border-gray-300" />
+                  </div>
+                  {/* กล่องสำหรับไอคอน */}
+                  <div className="flex justify-start items-center space-x-2 mt-2">
+                    <button className="text-brown-500 p-2">
+                      <img src="/icon/Bookmark.png" alt="Bookmark Icon" className="w-5 h-5" />
+                    </button>
+                    <button className="text-red-500 p-2">
+                      <img src="/icon/Trash.png" alt="Trash Icon" className="w-5 h-5" />
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         ))}
@@ -76,9 +97,9 @@ const CartView = () => {
 
       {/* ส่วนของการจัดการจัดส่ง */}
       <div className="mt-6">
-        <p className="font-bold">SHIPPING</p>
-        <p className="text-sm text-gray-500">ESTIMATED TIME : MONTHS DAYS</p>
-        <button className="text-accent-900 underline text-sm">EDIT LOCATION</button>
+        <p className="text-[#603F26] font-bold font-inter">SHIPPING</p>
+        <p className="font-inter text-gray-500">ESTIMATED TIME : MONTHS DAYS</p>
+        <button className="text-accent-900 underline text-sm font-bold font-inter">EDIT LOCATION</button>
       </div>
     </div>
   );
--- src/Components/PurchasePage/PurchasePage.jsx
+++ src/Components/PurchasePage/PurchasePage.jsx
@@ -4,14 +4,14 @@ import PurchaseSummary from './PurchaseSummary';
 
 const PurchasePage = () => {
   return (
-    <div className="flex p-8 justify-between bg-white items-start" style={{ height: 'calc(96vh - 50px)' }}>
+    <div className="flex flex-col lg:flex-row p-8 justify-between bg-white items-start h-full" style={{ height: 'calc(95vh - 50px)' }}>
       {/* ส่วนแสดงรายการสินค้า */}
-      <div className="w-3/5 bg-white rounded-lg p-4 overflow-y-auto"> 
+      <div className="w-full lg:w-2/3 bg-white rounded-lg p-4 overflow-y-auto flex-grow mb-4 lg:mb-0"> 
         <CartView />
       </div>
 
       {/* ส่วนสรุปยอดรวม */}
-      <div className="w-2/5 bg-white rounded-lg p-4">
+      <div className="w-full lg:w-1/3 bg-white rounded-lg p-4 lg:h-auto h-auto">
         <PurchaseSummary />
       </div>
     </div>
