922844d5fe3a4f62a211ae73dcaf3c2088148fab
n.thnptp <noppadol196@gmail.com>
2024-10-29 17:00:20 +0700

    ✨ added database wrapper & getdata for api testing

--- pages/api/getdata.js
+++ pages/api/getdata.js
@@ -1,5 +1,21 @@
 import mysql from "mysql2/promise";
 
 export default async function handler(req, res) {
-    res.status(200).json({ name: "John Doe" });
+    
+    const dbconnection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+
+    try {
+        const query = "SELECT * from admin"
+        const values = []
+        const [data] = await dbconnection.execute(query, values);
+        dbconnection.end();
+        res.status(200).json({ results: data });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
